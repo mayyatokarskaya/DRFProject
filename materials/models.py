eb5e205ff684e14e065d8ad544403e9e8cf0f45a
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-30 23:52:13 +1000

    creating a materials models.py

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    preview = models.ImageField(upload_to='courses/', blank=True, null=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    preview = models.ImageField(upload_to='lessons/', blank=True, null=True)
+    description = models.TextField()
+    video_url = models.URLField()
+
+    def __str__(self):
+        return self.title

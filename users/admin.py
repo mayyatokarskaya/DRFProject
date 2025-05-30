a765d0c7d6de36f5b25e1721526c02dd7bde33b8
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-31 00:05:03 +1000

    creating a admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('id', 'email', 'username', 'phone', 'city', 'is_staff', 'is_active')
+    search_fields = ('email', 'username', 'phone', 'city')
+    list_filter = ('is_staff', 'is_active', 'city')
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (None, {'fields': ('phone', 'city', 'avatar')}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (None, {'fields': ('phone', 'city', 'avatar')}),
+    )

d53cca21790748681c33171fc7d26a929d728acf
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-05-31 00:16:01 +1000

    create a ViewSet for courses

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
+from rest_framework import generics
 
-# Create your views here.
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class LessonListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer

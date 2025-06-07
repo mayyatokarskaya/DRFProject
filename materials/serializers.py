72b2f9cd7951a422646ecb5a102b2d5d5e8ae7f4
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-06-07 10:41:21 +1000

    implementation for serializer for course model output field lessons

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,18 +2,20 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    lessons_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-    def get_lessons_count(self, obj):
-        return obj.lessons.count()
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True, source='lessons.all')
+
+    class Meta:
+        model = Course
+        fields = ['id', 'title', 'preview', 'description', 'lessons_count', 'lessons']
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
+

dd7def692072dfaacfd442fc5fbbef1271d76cc5
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-06-07 10:55:49 +1000

    setting up a payment serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Payment
+from materials.serializers import CourseSerializer, LessonSerializer
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+    paid_course = CourseSerializer(read_only=True)
+    paid_lesson = LessonSerializer(read_only=True)
+
     class Meta:
         model = Payment
-        fields = "__all__"
+        fields = '__all__'

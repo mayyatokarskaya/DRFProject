09b7aa6d101c104d697b44d7fc4ccbfcaee7b691
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-06-08 05:59:55 +1000

    settings serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Payment, User
 from materials.serializers import CourseSerializer, LessonSerializer
+from django.contrib.auth.hashers import make_password
 
 
 class PaymentSerializer(serializers.ModelSerializer):
@@ -22,3 +23,32 @@ class UserProfileSerializer(serializers.ModelSerializer):
     def get_payment_history(self, obj):
         payments = obj.payments.all().order_by('-payment_date')
         return PaymentSerializer(payments, many=True).data
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'password2', 'phone', 'city']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError("Пароли не совпадают")
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create(
+            email=validated_data['email'],
+            password=make_password(validated_data['password']),
+            phone=validated_data.get('phone', ''),
+            city=validated_data.get('city', '')
+        )
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'phone', 'city', 'avatar']

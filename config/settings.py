99a247afd05146bbe96656504a1eaf3ca232fd31
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-06-07 10:53:42 +1000

    set up filtering for payments

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,17 @@ INSTALLED_APPS = [
     "rest_framework",
     "users",
     "materials",
+    "django_filters",
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        ...
+    ),
+}
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Payment
+from .serializers import PaymentSerializer
+from .filters import PaymentFilter
+from django_filters import rest_framework as django_filters
 
-# Create your views here.
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [django_filters.DjangoFilterBackend]
+    filterset_class = PaymentFilter

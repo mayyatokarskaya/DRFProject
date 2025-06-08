c75f5ce1bc3f7a6f79f0c81f9c471e3369740499
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-06-08 12:25:40 +1000

    Setting up URLs

--- materials/urls.py
+++ materials/urls.py
@@ -1,12 +1,19 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet, LessonListCreateAPIView, LessonRetrieveUpdateDestroyAPIView
+from .views import (
+    CourseViewSet,
+    LessonCreateAPIView,
+    LessonRetrieveUpdateAPIView,
+    LessonListAPIView,
+    LessonDestroyAPIView,
+)
 
 router = DefaultRouter()
 router.register(r"courses", CourseViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),
-    path("lessons/", LessonListCreateAPIView.as_view(), name="lesson-list-create"),
-    path("lessons/<int:pk>/", LessonRetrieveUpdateDestroyAPIView.as_view(), name="lesson-detail"),
-]
+    path("lessons/", LessonListAPIView.as_view(), name="lesson-list"),
+    path("lessons/create/", LessonCreateAPIView.as_view(), name="lesson-create"),
+    path("lessons/<int:pk>/", LessonRetrieveUpdateAPIView.as_view(), name="lesson-detail"),
+    path("lessons/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson-delete"),
+] + router.urls

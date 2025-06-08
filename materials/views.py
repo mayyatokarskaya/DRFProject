7ee7d87ba116a95141709675d76e84a93a5ee097
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-06-08 12:07:04 +1000

    Setting up rights for courses

--- materials/views.py
+++ materials/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+
+from users.permissions import IsModerator
 from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
 from rest_framework import generics
@@ -7,6 +10,14 @@ from rest_framework import generics
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    def get_permissions(self):
+        """Разделение прав по действиям"""
+        if self.action in ['create', 'destroy']:
+            return [IsAdminUser()]
+        elif self.action in ['update', 'partial_update', 'retrieve', 'list']:
+            return [IsAuthenticated(), IsModerator() | IsAdminUser()]
+        return [IsAdminUser()]
 
 
 class LessonListCreateAPIView(generics.ListCreateAPIView):

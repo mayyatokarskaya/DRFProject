c2d8cdf36cea77b01ae10ad23b5f7d4b4f0b9863
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-06-15 09:33:58 +1000

    creating a payment model

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "materials",
     "django_filters",
     "drf_yasg",
+    'payments',
 ]
 
 
--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from materials.models import Course
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    stripe_session_id = models.CharField(max_length=255)
+    stripe_payment_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)

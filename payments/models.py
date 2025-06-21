94e31a7a62e96c3106a43ffe7f16bd01cbe40b70
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-06-21 22:06:43 +1000

    correction of files

--- payments/models.py
+++ payments/models.py
@@ -6,7 +6,14 @@ User = get_user_model()
 
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="stripe_payments"  # 👈 Уникальное имя для связи
+    )
     stripe_session_id = models.CharField(max_length=255)
     stripe_payment_url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.course} - {self.created_at}"

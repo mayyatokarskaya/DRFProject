df846fd49f0eb4b6a9b1a9c1b1ec81cf16f6ffd8
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-06-21 22:43:27 +1000

    adding price to rate

--- materials/models.py
+++ materials/models.py
@@ -7,6 +7,8 @@ class Course(models.Model):
     title = models.CharField(max_length=255, verbose_name="Название")
     preview = models.ImageField(upload_to="courses/", blank=True, null=True, verbose_name="Превью")
     description = models.TextField(verbose_name="Описание")
+    owner = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    price = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
